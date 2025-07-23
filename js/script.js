c34b4a690b028ff804d6bcb2082d73b63e87f7be
Serge <serge-mur@yandex.ru>
2025-07-23 17:40:57 +0300

    day 2

--- js/script.js
+++ js/script.js
@@ -2,4 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log('DOMContentLoaded');
 
+    const topSlider = new Swiper('.top-slider', {
+        navigation: {
+          nextEl: '.top__next',
+          prevEl: '.top__prev',
+        },
+    });
+
+    const categorySlider = new Swiper('.category-slider', {
+        slidesPerView: 1.1,
+        spaceBetween: 20,
+        breakpoints: {
+            576: {
+                slidesPerView: 2.1,
+            },
+            1200: {
+                slidesPerView: 4.1,
+            },
+        },
+        navigation: {
+          nextEl: '.category-slider__next',
+          prevEl: '.category-slider__prev',
+        },
+    });
+
 });
