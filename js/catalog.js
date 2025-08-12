41d76ad1194d948f42b901680e241b1a85972fb6
Serge <serge-mur@yandex.ru>
2025-08-12 09:20:07 +0300

    filter

--- js/catalog.js
+++ js/catalog.js
@@ -2,6 +2,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log('DOMContentLoaded');
 
+    /* offcanvas info window */
+    const filter = document.querySelector('.filter');
+    const filterToggle = document.querySelector('.control__filter');
+    const filterClose = document.querySelector('.filter__close');
+    const filterOverlay = document.querySelector('.filter__overlay');
+    const filterWrapper = document.querySelector('.filter__wrapper');
+    function toggleFilter() {
+        filterToggle.addEventListener('click', () => {
+            filter.classList.add('filter_open');
+            document.body.classList.add("remove-scrolling");
+        });
+        filterClose.addEventListener('click', () => {
+            filter.classList.remove('filter_open');
+            document.body.classList.remove("remove-scrolling");
+        });
+        filterOverlay.addEventListener('click', () => {
+            filter.classList.remove('filter_open');
+            document.body.classList.remove("remove-scrolling");
+        });
+    }
+    toggleFilter();
+
+
+    document.querySelector('.sorting__button').addEventListener('click', function(el) {
+        el.currentTarget.classList.toggle('sorting__button_open');
+        el.target.closest('.sorting').querySelector('.sorting__content').classList.toggle('sorting__content_open');
+    });    
+
     document.querySelectorAll('.card-slider').forEach(el => {
         new Swiper(el, {
             slidesPerView: 'auto',
@@ -15,8 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // fix to all
     document.querySelector('.card-image__toggle').addEventListener('click', function(el) {
-        // console.log(el.target);
         el.target.classList.toggle('card-image__toggle_open');
         el.target.closest('.card-image').querySelector('.card-image__list').classList.toggle('card-image__list_open');
     });
