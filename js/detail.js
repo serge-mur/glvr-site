69d51ddfd35ca7720e27c49ab9220ce22a02149d
Serge <serge-mur@yandex.ru>
2025-09-09 15:30:05 +0300

    offcanvas

--- js/detail.js
+++ js/detail.js
@@ -47,6 +47,28 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // offcanvas
+    const offcanvasLink = document.querySelectorAll('.offcanvas-link');
+    offcanvasLink.forEach(link => {
+        const target = link.dataset.target;
+        const offcanvas = document.getElementById(target);
+        const offcanvasClose = offcanvas.querySelector('.offcanvas__close');
+        const offcanvasOverlay = offcanvas.querySelector('.offcanvas__overlay');
+        const offcanvasWrapper = offcanvas.querySelector('.offcanvas__wrapper');      
+        link.addEventListener('click', () => {
+            offcanvas.classList.add('offcanvas_open');
+            document.body.classList.add("remove-scrolling");
+        });
+        offcanvasClose.addEventListener('click', () => {
+            offcanvas.classList.remove('offcanvas_open');
+            document.body.classList.remove("remove-scrolling");
+        });
+        offcanvasOverlay.addEventListener('click', () => {
+            offcanvas.classList.remove('offcanvas_open');
+            document.body.classList.remove("remove-scrolling");
+        });
+    });
+
 
 
 
